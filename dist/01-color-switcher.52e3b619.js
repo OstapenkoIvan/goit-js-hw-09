2aa68c3c214f265f8767926a36adf2297039b6a4
OstapenkoIvan <ostapenko.ivan@gmail.com>
2022-08-21 14:04:10 +0300

    1

--- dist/01-color-switcher.52e3b619.js
+++ dist/01-color-switcher.52e3b619.js
@@ -503,6 +503,29 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"cYUEh":[function(require,module,exports) {
+const refs = {
+    bodyEl: document.querySelector("body"),
+    startButton: document.querySelector("[data-start]"),
+    stopButton: document.querySelector("[data-stop]")
+};
+let colorInterval = null;
+refs.bodyEl.addEventListener("click", changeBackgroundColor);
+function changeBackgroundColor(evt) {
+    if (evt.target.nodeName !== "BUTTON") return;
+    if ("start" in evt.target.dataset) {
+        refs.startButton.setAttribute("disabled", "");
+        colorInterval = setInterval(()=>{
+            refs.bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+        }, 1000);
+    }
+    if ("stop" in evt.target.dataset) {
+        refs.startButton.removeAttribute("disabled");
+        clearInterval(colorInterval);
+    }
+}
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
 },{}]},["gzrwE","cYUEh"], "cYUEh", "parcelRequired7c6")
 
