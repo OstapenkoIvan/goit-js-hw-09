3b2a607e168ce5e3307c4ee56ea20daa3a3a9820
OstapenkoIvan <75904652+OstapenkoIvan@users.noreply.github.com>
2022-08-21 11:52:28 +0000

    Deploying to gh-pages from @ OstapenkoIvan/goit-js-hw-09@34d5e2603b542a718d0cb6dc039bf0021edb9818 🚀

---
+++ 01-color-switcher.bb5f9663.js
@@ -0,0 +1,2 @@
+const t={bodyEl:document.querySelector("body"),startButton:document.querySelector("[data-start]"),stopButton:document.querySelector("[data-stop]")};let e=null;t.bodyEl.addEventListener("click",(function(o){if("BUTTON"!==o.target.nodeName)return;"start"in o.target.dataset&&(t.startButton.setAttribute("disabled",""),e=setInterval((()=>{t.bodyEl.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3));"stop"in o.target.dataset&&(t.startButton.removeAttribute("disabled"),clearInterval(e))}));
+//# sourceMappingURL=01-color-switcher.bb5f9663.js.map
